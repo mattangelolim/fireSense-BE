1f5390d3b01dcc4f992c7a73b0519115f903061b
mattangeloblim <matt.lim@breddas.com.ph>
2023-11-13 13:36:25 +0800

    commit

--- routes/movingAverageRoutes.js
+++ routes/movingAverageRoutes.js
@@ -50,4 +50,53 @@ router.get("/moving-average", async (req, res) => {
   }
 });
 
+router.get("/moving-average/prediction", async (req,res) =>{
+  try {
+    const casesPerYear = await Case.findAll()
+    const totalCountsPerYear = {};
+
+    // Loop through cases and increment total counts
+    casesPerYear.forEach((caseData) => {
+      const year = caseData.year;
+      const count = caseData.count;
+
+      // Initialize count if not already present for the year
+      totalCountsPerYear[year] = totalCountsPerYear[year] || { count: 0 };
+
+      // Increment total count
+      totalCountsPerYear[year].count += count;
+    });
+
+    const linearRegression = (x, y, predictX) => {
+      // Calculate the mean of arrays x and y
+      const meanX = x.reduce((acc, val) => acc + val, 0) / x.length;
+      const meanY = y.reduce((acc, val) => acc + val, 0) / y.length;
+    
+      // Calculate the coefficients a and b of the linear regression equation (y = a * x + b)
+      const numerator = x.reduce((acc, val, i) => acc + (val - meanX) * (y[i] - meanY), 0);
+      const denominator = x.reduce((acc, val) => acc + Math.pow(val - meanX, 2), 0);
+      const a = numerator / denominator;
+      const b = meanY - a * meanX;
+    
+      // Predict the value for predictX
+      const predictY = a * predictX + b;
+    
+      return predictY;
+    };
+    
+    
+    // Extract years and counts from the data
+    const years = Object.keys(totalCountsPerYear).map(Number);
+    const counts = Object.values(totalCountsPerYear).map((entry) => entry.count);
+    
+    // Predict the value for 2024
+    const predictedValue = linearRegression(years, counts, 2024);
+
+    res.json(predictedValue)
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: error.message})
+  }
+})
 module.exports = router;
