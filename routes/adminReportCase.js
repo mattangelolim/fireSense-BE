c7efa266299258b46981da9dbe69d97c3766e74f
Matt Lim <mattlim248@gmail.com>
2023-11-25 17:52:29 +0800

    message

---
+++ routes/adminReportCase.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/message", async, (req, res) => {
+    const { district } = req.body
+
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        res.status
+    }
+})
+
+module.exports = router
