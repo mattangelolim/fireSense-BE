94d36624940e827f412393de8218c1638abfa9c4
httpreq <mattlim248@gmail.com>
2023-12-27 12:12:10 +0800

    commit

--- routes/movingAverageRoutes.js
+++ routes/movingAverageRoutes.js
@@ -52,6 +52,76 @@ router.get("/moving-average", async (req, res) => {
   }
 });
 
+router.get("/moving-average/text", async (req, res) => {
+  const windowSize = 3;
+
+  try {
+    const movingAveragesByDistrict = {};
+
+    // Loop through each district (1 to 6)
+    for (let district = 1; district <= 6; district++) {
+      let totalMovingAverage = 0;
+
+      // Loop through each year (2017 to 2024)
+      for (let year = 2017; year <= 2023; year++) {
+        const cases = await Case.findAll({
+          where: {
+            year: year,
+            district: district,
+          },
+        });
+
+        const aggregatedData = {};
+        cases.forEach((caseData) => {
+          const month = caseData.month;
+          if (aggregatedData[month]) {
+            aggregatedData[month] += caseData.count;
+          } else {
+            aggregatedData[month] = caseData.count;
+          }
+        });
+
+        const aggregatedArray = Object.keys(aggregatedData).map((month) => ({
+          month: month,
+          count: aggregatedData[month],
+        }));
+
+        const movingAverages = [];
+        for (let i = 0; i <= aggregatedArray.length - windowSize; i++) {
+          const window = aggregatedArray.slice(i, i + windowSize);
+          const average =
+            window.reduce((sum, value) => sum + value.count, 0) / windowSize;
+          movingAverages.push(Math.round(average));
+        }
+
+        // Sum up moving averages for each district
+        totalMovingAverage += movingAverages.reduce((sum, value) => sum + value, 0);
+
+        // Calculate the average of moving averages for the current year and district
+        const averageMovingAverage = totalMovingAverage / (year - 2017 + 1);
+
+        if (!movingAveragesByDistrict[district]) {
+          movingAveragesByDistrict[district] = {};
+        }
+
+        movingAveragesByDistrict[district][year] = Math.round(averageMovingAverage);
+      }
+    }
+
+    // Perform text analysis and add analysis to the response
+    const analysisByDistrict = {};
+
+    for (const district in movingAveragesByDistrict) {
+      analysisByDistrict[district] = analyzeTrend(movingAveragesByDistrict[district]);
+    }
+
+    res.json({ analysisByDistrict });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 router.get("/moving-average/prediction", async (req, res) => {
 
   try {
@@ -105,6 +175,27 @@ router.get("/moving-average/prediction", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Function to analyze the trend based on moving averages
+function analyzeTrend(movingAverages) {
+  const trendAnalysis = {};
+
+  for (const year in movingAverages) {
+    const currentYearValue = movingAverages[year];
+    const previousYear = parseInt(year) - 1;
+    const previousYearValue = movingAverages[previousYear];
+
+    if (currentYearValue > previousYearValue) {
+      trendAnalysis[year] = "Increasing";
+    } else if (currentYearValue < previousYearValue) {
+      trendAnalysis[year] = "Decreasing";
+    } else {
+      trendAnalysis[year] = "Stable";
+    }
+  }
+
+  return trendAnalysis;
+}
 
 module.exports = router;
 
