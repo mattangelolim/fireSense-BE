defa0526a80950c15501f935462a7cc3eaa4e65d
mattangeloblim <matt.lim@breddas.com.ph>
2024-01-12 18:02:54 +0800

    commit alert

--- routes/adminAdvisoryRoutes.js
+++ routes/adminAdvisoryRoutes.js
@@ -15,10 +15,9 @@ router.post("/advisory", async (req, res) => {
     expirationMinutes,
     expirationSeconds,
     district,
-    alert,
+    areas,
+    alert
   } = req.body;
-
-
 
   try {
     const CurrentDate = moment()
@@ -31,9 +30,6 @@ router.post("/advisory", async (req, res) => {
       .add(expirationSeconds, "seconds")
       .format("YYYY-MM-DD HH:mm:ss");
 
-    // console.log(expirationDate);
-    console.log(alert)
-
     const newAdvisory = await Advisory.create({
       announcement,
       expirationDate,
@@ -41,23 +37,20 @@ router.post("/advisory", async (req, res) => {
       alert,
     });
 
-    if (alert === "Alert 3") {
+    if (alert === "FIRE OUT") {
       // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
       const users = await User.findAll({
         where: {
           phone: { [Op.not]: null },
-          // district: district,
-          // user_type: { [Op.not]: "admin" },
         },
       });
       const phone = users.map((user) => user.phone);
-      console.log(phone);
 
       // Send SMS notifications to registered users
       phone.forEach((phone) => {
         client.messages
           .create({
-            body: `A new announcement has posted "${announcement}" `,
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: FIRE HAS BEEN NEUTRALIZED `,
             from: "+12295261907",
             to: phone,
           })
@@ -69,23 +62,21 @@ router.post("/advisory", async (req, res) => {
           );
       });
     }
-    if (alert === "Alert 2") {
+
+    if (alert === "UNDER CONTROL") {
       // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
       const users = await User.findAll({
         where: {
           phone: { [Op.not]: null },
-          district: district,
-          // user_type: { [Op.not]: "admin" },
         },
       });
       const phone = users.map((user) => user.phone);
-      console.log(phone);
 
       // Send SMS notifications to registered users
       phone.forEach((phone) => {
         client.messages
           .create({
-            body: `A new announcement has posted "${announcement}" `,
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: DECREASING FIRE RISK `,
             from: "+12295261907",
             to: phone,
           })
@@ -97,25 +88,21 @@ router.post("/advisory", async (req, res) => {
           );
       });
     }
-    if (alert === "Alert 1") {
+
+    if (alert === "GENERAL ALARM") {
       // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
       const users = await User.findAll({
         where: {
           phone: { [Op.not]: null },
-          district: district,
-          user_type: {
-            [Op.or]: ["Firefighter", "Resident"],
-          },
         },
       });
       const phone = users.map((user) => user.phone);
-      console.log(phone);
 
       // Send SMS notifications to registered users
       phone.forEach((phone) => {
         client.messages
           .create({
-            body: `A new announcement has posted "${announcement}" `,
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: FIRE HAS AFFECTING MAJOR PART OF AREA `,
             from: "+12295261907",
             to: phone,
           })
@@ -127,7 +114,256 @@ router.post("/advisory", async (req, res) => {
           );
       });
     }
-    res.status(201).json({ advisory: newAdvisory});
+    if (alert === "TASK FORCE DELTA") {
+      // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+      const users = await User.findAll({
+        where: {
+          phone: { [Op.not]: null },
+        },
+      });
+      const phone = users.map((user) => user.phone);
+
+      // Send SMS notifications to registered users
+      phone.forEach((phone) => {
+        client.messages
+          .create({
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: FIRE HAS AFFECTING SIGNIFICANT PART OF AREA - 35 FIRE TRUCKS `,
+            from: "+12295261907",
+            to: phone,
+          })
+          .then((message) =>
+            console.log(`SMS sent to ${phone}: ${message.sid}`)
+          )
+          .catch((error) =>
+            console.error(`Error sending SMS to ${phone}:`, error)
+          );
+      });
+    }
+
+    if (alert === "TASK FORCE CHARLIE") {
+      // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+      const users = await User.findAll({
+        where: {
+          phone: { [Op.not]: null },
+        },
+      });
+      const phone = users.map((user) => user.phone);
+
+      // Send SMS notifications to registered users
+      phone.forEach((phone) => {
+        client.messages
+          .create({
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: FIRE HAS AFFECTING SIGNIFICANT PART OF AREA - 32 FIRE TRUCKS`,
+            from: "+12295261907",
+            to: phone,
+          })
+          .then((message) =>
+            console.log(`SMS sent to ${phone}: ${message.sid}`)
+          )
+          .catch((error) =>
+            console.error(`Error sending SMS to ${phone}:`, error)
+          );
+      });
+    }
+
+    if (alert === "TASK FORCE BRAVO") {
+      // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+      const users = await User.findAll({
+        where: {
+          phone: { [Op.not]: null },
+        },
+      });
+      const phone = users.map((user) => user.phone);
+
+      // Send SMS notifications to registered users
+      phone.forEach((phone) => {
+        client.messages
+          .create({
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE:  15 X 15 Houses AFFECTED - 28 Fire Trucks`,
+            from: "+12295261907",
+            to: phone,
+          })
+          .then((message) =>
+            console.log(`SMS sent to ${phone}: ${message.sid}`)
+          )
+          .catch((error) =>
+            console.error(`Error sending SMS to ${phone}:`, error)
+          );
+      });
+    }
+
+    if (alert === "TASK FORCE ALPHA") {
+      // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+      const users = await User.findAll({
+        where: {
+          phone: { [Op.not]: null },
+        },
+      });
+      const phone = users.map((user) => user.phone);
+      console.log(phone);
+
+      // Send SMS notifications to registered users
+      phone.forEach((phone) => {
+        client.messages
+          .create({
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: 12 X 12 Houses AFFECTED – 24 Fire Trucks`,
+            from: "+12295261907",
+            to: phone,
+          })
+          .then((message) =>
+            console.log(`SMS sent to ${phone}: ${message.sid}`)
+          )
+          .catch((error) =>
+            console.error(`Error sending SMS to ${phone}:`, error)
+          );
+      });
+    }
+
+    if (alert === "FIFTH ALARM") {
+      // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+      const users = await User.findAll({
+        where: {
+          phone: { [Op.not]: null },
+        },
+      });
+      const phone = users.map((user) => user.phone);
+      console.log(phone);
+
+      // Send SMS notifications to registered users
+      phone.forEach((phone) => {
+        client.messages
+          .create({
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: 10-11 Houses or High Rise Building AFFECTED - 20 Fire Trucks`,
+            from: "+12295261907",
+            to: phone,
+          })
+          .then((message) =>
+            console.log(`SMS sent to ${phone}: ${message.sid}`)
+          )
+          .catch((error) =>
+            console.error(`Error sending SMS to ${phone}:`, error)
+          );
+      });
+    }
+
+    if (alert === "FOURTH ALARM") {
+      // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+      const users = await User.findAll({
+        where: {
+          phone: { [Op.not]: null },
+        },
+      });
+      const phone = users.map((user) => user.phone);
+      console.log(phone);
+
+      // Send SMS notifications to registered users
+      phone.forEach((phone) => {
+        client.messages
+          .create({
+            body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: 8-9 Houses or High Rise Building AFFECTED - 16 Fire Trucks `,
+            from: "+12295261907",
+            to: phone,
+          })
+          .then((message) =>
+            console.log(`SMS sent to ${phone}: ${message.sid}`)
+          )
+          .catch((error) =>
+            console.error(`Error sending SMS to ${phone}:`, error)
+          );
+      });
+    }
+
+    if (areas) {
+      for (const currentArea of areas) {
+
+        if (alert === "THIRD ALARM") {
+          // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+          const users = await User.findAll({
+            where: {
+              phone: { [Op.not]: null },
+              district: district,
+              area: currentArea
+            },
+          });
+          const phone = users.map((user) => user.phone);
+          console.log(phone);
+  
+          // Send SMS notifications to registered users
+          phone.forEach((phone) => {
+            client.messages
+              .create({
+                body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: 6-7 Houses or High Rise Building - 12 Fire Trucks `,
+                from: "+12295261907",
+                to: phone,
+              })
+              .then((message) =>
+                console.log(`SMS sent to ${phone}: ${message.sid}`)
+              )
+              .catch((error) =>
+                console.error(`Error sending SMS to ${phone}:`, error)
+              );
+          });
+        }
+        if (alert === "SECOND ALARM") {
+          const users = await User.findAll({
+            where: {
+              phone: { [Op.not]: null },
+              district: district,
+              area: currentArea
+            },
+          });
+          const phone = users.map((user) => user.phone);
+          console.log(phone);
+  
+          // Send SMS notifications to registered users
+          phone.forEach((phone) => {
+            client.messages
+              .create({
+                body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: 4-5 Houses - 8 Fire Trucks. `,
+                from: "+12295261907",
+                to: phone,
+              })
+              .then((message) =>
+                console.log(`SMS sent to ${phone}: ${message.sid}`)
+              )
+              .catch((error) =>
+                console.error(`Error sending SMS to ${phone}:`, error)
+              );
+          });
+        }
+        if (alert === "FIRST ALARM") {
+          // FIND USERS THAT REGISTERED IN THE SYSTEM EXCEPT ADMIN
+          const users = await User.findAll({
+            where: {
+              phone: { [Op.not]: null },
+              district: district,
+              area: currentArea
+            },
+          });
+          const phone = users.map((user) => user.phone);
+          console.log(phone);
+  
+          // Send SMS notifications to registered users
+          phone.forEach((phone) => {
+            client.messages
+              .create({
+                body: `A new announcement has posted "${announcement}". PLEASE TAKE NOTE: 2-3 Houses - 4 Fire Trucks `,
+                from: "+12295261907",
+                to: phone,
+              })
+              .then((message) =>
+                console.log(`SMS sent to ${phone}: ${message.sid}`)
+              )
+              .catch((error) =>
+                console.error(`Error sending SMS to ${phone}:`, error)
+              );
+          });
+        }
+      }
+    }
+
+    
+    res.status(201).json({ advisory: newAdvisory });
   } catch (error) {
     console.error("Error creating advisory:", error);
     res.status(500).json({ error: "Internal Server Error" });
